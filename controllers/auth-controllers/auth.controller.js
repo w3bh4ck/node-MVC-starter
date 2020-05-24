6e8b198b0eaa4b0aaa2570ed30dec3a756ff15fd
Lucky Amadi <lucky@softcom.ng>
2020-05-24 02:17:30 +0100

    added backend validation structure

--- controllers/auth-controllers/auth.controller.js
+++ controllers/auth-controllers/auth.controller.js
@@ -1,3 +1,12 @@
+const { validationResult } = require("express-validator");
 exports.login = (req, res, next) => {
 	res.status(200).json(req.body);
+};
+
+exports.signup = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json(errors);
+	}
+	res.status(200).json({ ...req.body, Message: "sign up successfull" });
 };
--- routes/auth-routes/auth.routes.js
+++ routes/auth-routes/auth.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const { check } = require("express-validator");
 const authControllers = require("../../controllers/auth-controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/login", authControllers.login);
+
+router.post(
+	"/signup",
+	[check("email").trim().isEmail(), check("password").trim().isLength({ min: 6 })],
+	authControllers.signup
+);
 
 module.exports = router;
