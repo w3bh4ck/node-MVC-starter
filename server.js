6102477f4bbc89f47e8d600269570ef230d56376
Lucky Amadi <lucky@softcom.ng>
2020-06-14 07:22:24 +0100

    implemented morgan

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const authRoute = require("./routes/auth-routes/auth.routes");
 
@@ -13,6 +14,11 @@ dotenv.config({ path: "./config/env/config.env" });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Dev logger
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
 // Mount app routers
 app.use("/api/v1/auth", authRoute);
