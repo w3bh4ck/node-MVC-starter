bb85a72579d88fd29b765ea1f0efde6b245de05f
Lucky Amadi <lucky@softcom.ng>
2020-06-14 06:45:28 +0100

    modified routes paths

--- routes/auth-routes/auth.routes.js
+++ routes/auth-routes/auth.routes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const { check } = require("express-validator");
-const authControllers = require("../../controllers/auth-controllers/auth.controller");
+const authController = require("../../controllers/auth-controllers/auth.controller");
 
 const router = express.Router();
 
 router.post(
 	"/login",
 	[check("email").trim().isEmail(), check("password").trim().isLength({ min: 6 })],
-	authControllers.login
+	authController.login
 );
 
 router.post(
 	"/signup",
 	[check("email").trim().isEmail(), check("password").trim().isLength({ min: 6 })],
-	authControllers.signup
+	authController.signup
 );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const authRoute = require("./routes/auth-routes/auth.routes");
 
+const app = express();
+
+//instatiate port
+const port = process.env.PORT || 5000;
+
 // load env variables
 dotenv.config({ path: "./config/env/config.env" });
 
-const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
-app.use(authRoute);
+// Mount app routers
+app.use("/api/v1/auth", authRoute);
 
 app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} on port: ${port}`));
